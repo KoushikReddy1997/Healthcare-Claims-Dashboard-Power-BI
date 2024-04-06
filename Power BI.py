64563430215f87f0640ab1cfa678cb8d3c01b69b
KoushikReddy1997 <163482296+KoushikReddy1997@users.noreply.github.com>
2024-04-06 19:15:59 -0400

    Update Power BI.py

--- Power BI.py
+++ Power BI.py
@@ -2,25 +2,17 @@
 # coding: utf-8
 
 # In[1]:
-
-
 # Evaluating variables: Cleaning & Manipulation 
 
 
 # In[2]:
-
-
 # Loading the necessary Libraries
-
 import pandas as pd
 import numpy as np
 
 
 # In[3]:
-
-
 # Reading Datasets Medical, Pharma and Enrollment datasets
-
 Medical_df= pd.read_csv("C:/Users/koush/Downloads/Power BI Project/snapshot-data-medical.csv")
 print('Medical:',Medical_df.head())
 print("Shape:", Medical_df.shape)
@@ -35,8 +27,6 @@ print("Shape:", Enrollment_df.shape)
 
 
 # In[4]:
-
-
 # Check Duplicates:
 # For the given datasets there is are no Unique ID's or Patient Identifiaction Information.
 # So, to identify the duplicates we can consider a record is duplicate if all the column values matches for 2 or more records 
@@ -56,19 +46,9 @@ print("Shape:", Enrollment_df.shape)
 # There is no duplicates found in Medical & Enrollement data but around 1k duplicates found in Pharma data.
 
 
-# In[ ]:
-
-
-# Writing a funciton that can be applied to all checks for every variable
-
-def Ultimate_checks():
-
 
 # In[5]:
-
-
 # Check Missing values: if present replacing as 'Not Known'
-
 print('Medical_data NA: ', Medical_df.isna().sum())
 print('Pharma_data NA: ', Pharma_df.isna().sum())
 print('Enrollment_data NA: ', Enrollment_df.isna().sum())
@@ -79,13 +59,8 @@ for i,x in Medical_df['procedure_code_type'].items():
         Medical_df.loc[i, 'procedure_code_type']= 'Not Known'
         
     
-
-
 # In[6]:
-
-
 # Check Semicolons and Multiple values 
-
 # Usually multiple values are sepearted by (", or ; or :"), if there are multiple values then named as 'Multiple Entries'
 
 # Medical Data 
@@ -93,29 +68,21 @@ for col in Medical_df.columns:
     Medical_df[col] = Medical_df[col].astype(str)
     mask = Medical_df[col].str.contains(',' '|:' '|;', na=False)
     Medical_df.loc[mask, col] = 'Multiple Entries'
-
 # Pharma Data
 for col in Pharma_df.columns:
     Pharma_df[col] = Pharma_df[col].astype(str)
     mask = Pharma_df[col].str.contains(',' '|:' '|;', na=False)
     Pharma_df.loc[mask, col] = 'Multiple Entries'
-
 # Enroll Data    
 for col in Enrollment_df.columns:
     Enrollment_df[col] = Enrollment_df[col].astype(str)
     mask = Enrollment_df[col].str.contains(',' '|:' '|;', na=False)
     Enrollment_df.loc[mask, col] = 'Multiple Entries'
     
-    
-
 
 # In[7]:
-
-
 # Check Outliers (outside ranges/actual list, Impossible dates)
-
 # Identifying if year Not in 2018-2021, then replacing it by 'Incorrect Year' 
-
 for i, x in Medical_df['reporting_year'].items():
     if str(x) not in ('2018','2019','2020','2021'):
         Medical_df.loc[i,'reporting_year'] = 'Incorrect Date'
@@ -129,23 +96,9 @@ for i, x in Enrollment_df['reporting_year'].items():
         Enrollment_df.loc[i,'reporting_year'] = 'Incorrect Date'
 
 
-# In[8]:
-
-
-# Check In-Correct data formats(zeros or presence of one data type in another )
-
-
-# In[9]:
-
-
-# Data Entry errors (typos, sepllings, captials)
-
 
 # In[10]:
-
-
 # Check White Spaces 
-
 for col in Medical_df.columns:
     Medical_df[col] = Medical_df[col].astype(str).str.strip()
     
@@ -157,8 +110,6 @@ for col in Enrollment_df.columns:
 
 
 # In[11]:
-
-
 # Standardization- Captializing all the strings for easy readability and to avoid any isssues for merging if needed later
 
 for col in Medical_df.columns:
@@ -172,8 +123,6 @@ for col in Enrollment_df.columns:
 
 
 # In[18]:
-
-
 # Renamings/ Replaceing terms (repalcing blanks- Not Known)
 # Renaming the Payer type to avoid the confusion
 
@@ -184,15 +133,6 @@ Medical_df= Medical_df.replace({
 
 
 # In[20]:
-
-
 # Saving the cleaned files to local for Data Visualization 
-
 Enrollment_df= Enrollment_df.to_csv('C:\Users\koush\Downloads\Power BI Project', index=False)
-
-
-# In[ ]:
-
-
-
 
